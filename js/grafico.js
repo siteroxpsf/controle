e63c68d2477dcc66328ef34f87c290a91ad6fd6d
siteroxpsf <carlosalexandre.psf@gmail.com>
2026-05-30 16:08:14 -0300

    Update grafico.js

--- js/grafico.js
+++ js/grafico.js
@@ -3,36 +3,32 @@ let grafico;
 export function atualizarGrafico(categorias){
 
     const ctx =
-    document.getElementById("grafico");
+        document.getElementById("grafico");
 
     if(grafico){
         grafico.destroy();
     }
 
-    grafico = new Chart({
+    grafico = new Chart(ctx,{
 
         type:"doughnut",
 
         data:{
-
             labels:Object.keys(categorias),
 
             datasets:[{
-
                 data:Object.values(categorias),
 
                 backgroundColor:[
-
                     "#16a34a",
                     "#dc2626",
                     "#7c3aed",
                     "#2563eb",
-                    "#f59e0b"
-
+                    "#f59e0b",
+                    "#14b8a6",
+                    "#f97316"
                 ]
-
             }]
-
         }
 
     });
