d59201a60b4e6bce39460a0d6694082f6f114f49
siteroxpsf <carlosalexandre.psf@gmail.com>
2026-05-30 16:08:29 -0300

    Update cartao.js

---
+++ js/cartao.js
@@ -0,0 +1,15 @@
+export function abrirCartao(){
+
+    document
+    .getElementById("modalCartao")
+    .style.display = "flex";
+
+}
+
+export function fecharCartao(){
+
+    document
+    .getElementById("modalCartao")
+    .style.display = "none";
+
+}
